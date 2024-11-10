cb5f8081f9c3f16a4e2e3ccd7b6553db8900eea4
ya7on <hello@syoma.website>
2024-11-10 21:53:00 +0300

    feat: init structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+mod api;
+mod conf;
+mod graphql;
+
+use crate::api::graphql::index_graphql;
+use crate::conf::conf;
+use crate::graphql::hello_world::HelloWorldQuery;
+use actix_web::{web, App, HttpServer};
+use async_graphql::{EmptyMutation, EmptySubscription, Schema};
+use async_graphql_actix_web::GraphQL;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let c = conf();
+
+    HttpServer::new(|| {
+        let schema = Schema::build(HelloWorldQuery, EmptyMutation, EmptySubscription).finish();
+
+        App::new()
+            .service(web::scope("/graphql").route("", web::get().to(index_graphql)))
+            .route("/", web::post().to(GraphQL::new(schema)))
+    })
+    .bind((c.host.as_str(), c.port))?
+    .run()
+    .await
 }
